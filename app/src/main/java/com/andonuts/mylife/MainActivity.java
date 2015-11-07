dd018bc9cc36ab3cb66e619574c9616f6b31b2c8
Andonuts <gowingjm@gmail.com>
2015-11-06 21:52:00 -0500

    Setting up test data.

--- app/src/main/java/com/andonuts/mylife/MainActivity.java
+++ app/src/main/java/com/andonuts/mylife/MainActivity.java
@@ -1,9 +1,11 @@
 package com.andonuts.mylife;
 
+import android.content.Context;
 import android.net.Uri;
 import android.os.Bundle;
 import android.support.design.widget.FloatingActionButton;
 import android.support.design.widget.Snackbar;
+import android.util.Log;
 import android.view.View;
 import android.support.design.widget.NavigationView;
 import android.support.v4.view.GravityCompat;
@@ -15,6 +17,11 @@ import android.view.Menu;
 import android.view.MenuItem;
 import android.widget.Toast;
 
+import java.io.BufferedReader;
+import java.io.File;
+import java.io.FileOutputStream;
+import java.io.FileReader;
+
 public class MainActivity extends AppCompatActivity
         implements NavigationView.OnNavigationItemSelectedListener,
         ChainListFragment.OnFragmentInteractionListener {
@@ -23,6 +30,43 @@ public class MainActivity extends AppCompatActivity
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        /*
+        TODO:  Do these things
+            1. Create Strings for chain data
+            2. Open the chain file
+            3. Save my string data to file overwriting all data inside
+            4. Output file data to Log to confirm it worked
+            5. Figure out steps to display file data in the ListFragment
+         */
+        String chainOne = "{\"Title\": \"Exercise\",\"StartDate\": \"2015-10-01\",\"EndDate\": \"2015-11-01\",\"Type\": \"MinMax\",\"MinDays\": 2,\"MaxDays\": \"4\",\"PerWeekValue\": null,\"Dates\": {\"2015-10-01\": \"D\",\"2015-10-02\": \"D\",\"2015-10-05\": \"S\",\"2015-10-10\": \"D\"}}";
+        String chainTwo = "{\"Title\": \"Handstands\",\"StartDate\": \"2015-10-01\",\"EndDate\": \"null\",\"Type\": \"PerWeek\",\"MinDays\": null,\"MaxDays\": null,\"PerWeekValue\": \"4\",\"Dates\": {\"2015-10-01\": \"D\",\"2015-10-02\": \"D\",\"2015-10-05\": \"S\",\"2015-10-10\": \"D\"}}";
+        String fileName = "chain.data";
+
+        // TRY TO WRITE TEST DATA TO FILE
+        try {
+            FileOutputStream fileOutputStream = openFileOutput(fileName, Context.MODE_PRIVATE);
+            String writeString = chainOne + "\n" + chainTwo;
+            fileOutputStream.write(writeString.getBytes());
+            fileOutputStream.close();
+            Log.d("TestChainData", "Done Writing data to " + fileName);
+        } catch (Exception e) {
+            Log.d("TestChainData", "Problem writing data to : " + fileName + e.toString());
+        }
+
+        // TRY TO READ TEST DATA TO CONFIRM IT IS THERE
+        try {
+            File file = new File(getFilesDir(), fileName);
+            BufferedReader bufferedReader = new BufferedReader(new FileReader(file));
+            String line;
+
+            while ((line = bufferedReader.readLine()) != null) {
+                Log.d("TestChainData", line);
+            }
+        } catch(Exception e) {
+            Log.e("TestChainData", "Trying to read the data");
+        }
+
 
         if (getFragmentManager().findFragmentById(R.id.content_area) == null) {
             ChainListFragment chainListFragment = new ChainListFragment();
