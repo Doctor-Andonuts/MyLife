869a735721eed7d311473b3e2e38e58f113c2c62
Andonuts <gowingjm@gmail.com>
2015-11-07 21:31:39 -0500

    Button in ListView

--- app/src/main/java/com/andonuts/mylife/CustomArrayAdapter.java
+++ app/src/main/java/com/andonuts/mylife/CustomArrayAdapter.java
@@ -5,7 +5,9 @@ import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
 import android.widget.ArrayAdapter;
+import android.widget.Button;
 import android.widget.TextView;
+import android.widget.Toast;
 
 import java.util.List;
 
@@ -23,13 +25,25 @@ public class CustomArrayAdapter extends ArrayAdapter<Chain> {
     }
 
     @Override
-    public View getView(int position, View convertView, ViewGroup parent) {
+    public View getView(final int position, View convertView, ViewGroup parent) {
         LayoutInflater inflater = (LayoutInflater) context
                 .getSystemService(Context.LAYOUT_INFLATER_SERVICE);
         View rowView = inflater.inflate(R.layout.chain_list_item, parent, false);
 
         TextView descriptionTextView = (TextView) rowView.findViewById(R.id.description);
         descriptionTextView.setText(chains.get(position).getTitle());
+
+        Button button= (Button) rowView.findViewById(R.id.button);
+        button.setOnClickListener(new View.OnClickListener()
+        {
+            @Override
+            public void onClick(View v)
+            {
+                Toast.makeText(context, "Clicked Button: " + chains.get(position).getTitle() , Toast.LENGTH_SHORT).show();
+                // Your code that you want to execute on this button click
+            }
+
+        });
 
         return rowView;
     }
